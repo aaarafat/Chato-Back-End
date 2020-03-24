b001274ee31085d2985ce8611e19ec325c90dd3d
Darkin <darkinb255@gmail.com>
2020-03-24 20:18:52 +0200

    auth returns token

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
-const Joi = require('joi');
+const Joi = require('@hapi/joi');
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 const {User} = require('../models/user');
 const bcrypt = require('bcrypt');
-
+const fs = require('fs');
 
 router.post('/',async (req,res)=>{
     const {error} = validate(req.body);
@@ -15,14 +16,16 @@ router.post('/',async (req,res)=>{
      let valid = await bcrypt.compare(req.body.password, user.password);
      if(!valid)
         return res.status(400).send("Invalid Email or Password");
-    res.send("Welcome");
+    const private = await fs.readFileSync(__dirname + '/../config/private.key','utf8')
+    console.log(private);
+    res.send(jwt.sign({_id:user._id},private));
 })
 
 function validate(user){
-    const schema = {
+    const schema = Joi.object({
         email:Joi.string().min(6).max(255).required().email(),
         password:Joi.string().min(5).max(255).required(),
-    }
-    return Joi.validate(user,schema);
+    })
+    return schema.validate(user);
 }
 module.exports = router;
