0437e80e01cf8b582eed134e77544e4a1f80d4dc
Darkin <darkinb255@gmail.com>
2020-03-24 17:12:01 +0200

    hash password

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const {User,validate} = require('../models/user');
+const bcrypt = require('bcrypt');
 
 
 router.get('/',async (req,res)=>{
@@ -18,6 +19,8 @@ router.post('/',async (req,res)=>{
     if(user)
         return res.status(400).send("This email is already registered ");
     user = User(_.pick(req.body,['name','email','password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password ,salt);
     user = await user.save();
     res.send(_.pick(user,['_id','name','email']));
    
