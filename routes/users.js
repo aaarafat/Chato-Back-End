6bb8ce875bd2de20b373e200c874759c2c83767e
Darkin <darkinb255@gmail.com>
2020-03-24 16:34:02 +0200

    users route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,26 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
-let users = [{name:"darkin" , email:"da@db.com"},{name:"lido22" , email:"aw@db.com"},{name:"boz" , email:"az@db.com"}]
+const {User,validate} = require('../models/user');
 
-router.get('/',(req,res)=>{
+
+router.get('/',async (req,res)=>{
+    const users = await User.find();
     res.send(users);
+})
+
+router.post('/',async (req,res)=>{
+    
+    const {error} = validate(req.body);
+    if(error)
+        return res.status(400).send(error.details[0].message);
+    let user = await User.findOne({email:req.body.email});
+    if(user)
+        return res.status(400).send("This email is already registered ");
+    user = User(_.pick(req.body,['name','email','password']));
+    user = await user.save();
+    res.send(_.pick(user,['_id','name','email']));
+   
 })
 
 module.exports = router;
