5c0be121e5cbf3b862fc1f7ff3f8f7fa50d8b07c
Darkin <darkinb255@gmail.com>
2020-03-24 16:33:54 +0200

    user model

--- models/user.js
+++ models/user.js
@@ -2,11 +2,35 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 const express = require('express');
 const User = mongoose.model('USer', new mongoose.Schema({
-name:{
-    type:String,
-    required:true,
-    minlength:6,
-    maxlength:20
+    name:{
+        type:String,
+        required:true,
+        minlength:5,
+        maxlength:50
+    } ,
+    email:{
+        type:String,
+        required:true,
+        minlength:6,
+        maxlength:255,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8,
+        maxlength:1024
+    }
+    }
+    ));
+function validateUser(user){
+    const schema = {
+        name:Joi.string().min(5).max(50).required(),
+        email:Joi.string().min(6).max(255).required().email(),
+        password:Joi.string().min(5).max(255).required(),
+    }
+    return Joi.validate(user,schema);
 }
 
-}));
+exports.User = User;
+exports.validate = validateUser;
