3045eb3f1b6f14a966b67982a3895e12c2c5267b
Darkin <darkinb255@gmail.com>
2020-03-24 20:18:33 +0200

    replace joi

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
 const express = require('express');
 const User = mongoose.model('USer', new mongoose.Schema({
@@ -24,12 +24,12 @@ const User = mongoose.model('USer', new mongoose.Schema({
     }
     ));
 function validateUser(user){
-    const schema = {
+    const schema = Joi.object({
         name:Joi.string().min(5).max(50).required(),
         email:Joi.string().min(6).max(255).required().email(),
         password:Joi.string().min(5).max(255).required(),
-    }
-    return Joi.validate(user,schema);
+    })
+    return schema.validate(user);
 }
 
 exports.User = User;
