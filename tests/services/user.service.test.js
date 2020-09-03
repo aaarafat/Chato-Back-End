8873eef8c6bba8ac54a60865f26cc3e43b244d5d
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-09-03 14:16:35 +0200

    Change password service test

--- tests/services/user.service.test.js
+++ tests/services/user.service.test.js
@@ -1,12 +1,13 @@
 const mockingoose = require('mockingoose').default;
 const { User } = require('../../src/models');
 const { userMock } = require('../mocks/models');
-const { userService } = require('../../src/services');
+const { userService, authService } = require('../../src/services');
 
 describe('User Service', () => {
     let user;
     beforeEach(() => {
         user = userMock.createFakeUser();
+        user.save = jest.fn().mockResolvedValue(user);
         mockingoose(User)
             .toReturn(user, 'findOne')
             .toReturn([user], 'find');
@@ -23,6 +24,33 @@ describe('User Service', () => {
         it('should return users', async () => {
             const result = await userService.getUsersByName('1', 20, 10);
             expect(result.length).toBeGreaterThan(0);
+        });
+    });
+
+    describe('changePassword', () => {
+        beforeEach(() => {
+            authService.hashPassword = jest.fn().mockImplementationOnce(() => 'hashedPassword');
+        });
+
+        it('should call authService hashPassword', async () => {
+            await userService.changePassword(user, 'newPassword');
+            expect(authService.hashPassword).toHaveBeenCalled();
+        });
+
+        it('should call authService hashPassword with newPassword', async () => {
+            const newPassword = 'newPassword';
+            await userService.changePassword(user, newPassword);
+            expect(authService.hashPassword).toHaveBeenCalledWith(newPassword);
+        });
+
+        it('should save the user', async () => {
+            await userService.changePassword(user, 'newPassword');
+            expect(user.save).toHaveBeenCalled();
+        });
+
+        it('should return user with hashedPassword', async () => {
+            const result = await userService.changePassword(user, 'newPassword');
+            expect(result.password).toBe('hashedPassword');
         });
     });
 });
