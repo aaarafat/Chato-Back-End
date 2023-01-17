de3b74b39e393fddadaa00f26738a929b7d2839d
fuboki10 <abdelrahman.tarek1910@gmail.com>
2023-01-17 11:50:02 +0200

    Fix testin in user controller

--- tests/controllers/user.controller.test.js
+++ tests/controllers/user.controller.test.js
@@ -1,7 +1,7 @@
-const {userService} = require('../../src/services');
-const {userController} = require('../../src/controllers');
-const {userMock} = require('../mocks/models');
-const {requestMock} = require('../mocks');
+const { userService } = require('../../src/services');
+const { userController } = require('../../src/controllers');
+const { userMock } = require('../mocks/models');
+const { requestMock } = require('../mocks');
 
 describe('User Controller', () => {
   let user; let req; let res;
@@ -11,7 +11,7 @@ describe('User Controller', () => {
       user = userMock.createFakeUser();
       userService.getUserById = jest.fn().mockImplementationOnce(() => user);
       res = requestMock.mockResponse();
-      req = {params: {id: '1'}};
+      req = { params: { id: '1' } };
     });
 
     it('should call getUserById service', async () => {
@@ -38,7 +38,8 @@ describe('User Controller', () => {
     it('should return user if valid', async () => {
       await userController.getUserById(req, res);
       user = user.toObject();
-      user.friends = undefined;
+      delete user.friends;
+      delete user.conversations;
       expect(res.json.mock.calls[0][0].user).toEqual(user);
     });
   });
